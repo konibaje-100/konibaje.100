1759ab15ef3f9b33d8ca8fc0d0c8d1b0cd82c0a3
konibaje-100 <konibajebrand@gmail.com>
2024-12-04 02:46:46 +0100

    production v3.1

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import { Route, Routes } from "react-router-dom";
-import Add from "./pages/add";
+import Add from "./pages/Add";
 import List from "./pages/List";
 import Orders from "./pages/Orders";
 import Login from "./components/Login";
