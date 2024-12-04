c60d008628de5890850a25d0a648faee4968064f
konibaje-100 <konibajebrand@gmail.com>
2024-12-04 02:41:06 +0100

    production v3

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/* import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -6,3 +6,4 @@ export default defineConfig({
   plugins: [react()],
   server: {port: 5174}
 })
+ */
--- backend/server.js
+++ backend/server.js
@@ -10,22 +10,22 @@ import orderRouter from './routes/orderRoute.js'
 
 // App config
 const app = express()
-const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
 
-// Middlewares 
+// Middlewares
 app.use(express.json())
 app.use(cors())
 
-// Api endpoins
-app.use('/api/user',userRouter)
-app.use('/api/product',productRouter)
-app.use('/api/cart',cartRouter)
-app.use('/api/order',orderRouter)
+// API endpoints
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/order', orderRouter)
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("API Working")
 })
 
-app.listen(port, ()=> console.log('Server started on PORT : '+ port))
+// Export the app for Vercel serverless functions
+export default app
