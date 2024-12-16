e1684446193dcf85e439ea4e3db56e5e34a000ed
konibaje-100 <konibajebrand@gmail.com>
2024-12-16 03:17:15 +0100

    address

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
           <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
           <div className='flex flex-col justify-center items-start gap-6'>
               <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-              <p className=' text-gray-500'>54709 Willms Station <br /> Ikate Lekki, Lagos, Nigeria </p>
+              <p className=' text-gray-500'>Ikate Lekki,<br /> Lagos, Nigeria </p>
               <p className=' text-gray-500'>Tel: +23400000000 <br /> Email: admin@konibaje100.com </p>
           </div>
       </div>
